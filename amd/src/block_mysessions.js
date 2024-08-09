1778f9c39807cf0beb77ad2fde859008bec5bbdd
Abdelilah EL OUARDI <abdelilah.elouardi@cgi.com>
2024-08-09 15:43:49 +0200

    Initial commit

---
+++ amd/src/block_mysessions.js
@@ -0,0 +1,178 @@
+/**
+ * Javascript containing function of the block mysessions
+ */
+define([
+    'jquery',
+    'local_mentor_core/pagination',
+    "format_edadmin/format_edadmin"
+], function ($, pagination, format_edadmin) {
+    var block_mysessions = {
+        /**
+         * Init JS
+         */
+        init: function (showsessioncompleted) {
+
+            // Replace session list header title in training sheet
+            $('.block_mysessions .training-sheet .list-sessions .target-header-title')
+                .text(M.util.get_string('session', 'block_mysessions'));
+
+            // Init session block pagination : 12 sessions per page
+            // If showsessioncompleted is :
+            // - True : Show all session.
+            // - False : Not show session completed.
+            this.initPagination(showsessioncompleted ? '.block-session-tile' : '.block-session-tile:not(.is-completed)');
+
+            // Redirect link event.
+            $('.block_mysessions .session-more-information').on('click', function (event) {
+                event.preventDefault();
+                event.stopPropagation();
+                var sessionId = $(event.currentTarget).data().sessionId;
+                window.location.href = M.cfg.wwwroot + '/local/trainings/pages/training.php?sessionid=' + sessionId;
+            });
+
+            $('#input-show-completed-session')
+                .click(function (e) {
+                    // Update user preference.
+                    $.ajax({
+                        url: M.cfg.wwwroot + '/local/user/ajax/ajax.php',
+                        data: {
+                            controller: 'user',
+                            action: 'set_user_preference',
+                            preferencename: 'block_mysessions_completed',
+                            value: $(e.target).is(':checked') ? '1' : '0'
+                        }
+                    });
+
+                    // Reset CSS display value.
+                    $('.block_mysessions .block-session-tile').hide();
+                    $('#mysessions-pagination').show();
+
+                    // Reset session block pagination : 12 sessions per page
+                    // If .is(':checked') is :
+                    // - True : Show all session.
+                    // - False : Not show session completed.
+                    block_mysessions.initPagination(
+                        $(e.target).is(':checked') ? '.block-session-tile' : '.block-session-tile:not(.is-completed)'
+                    );
+                })
+                // Set checkbox value with user preference.
+                .prop('checked', showsessioncompleted);
+
+            // Favourite action
+            $('.block_mysessions .fav').on('click', function (event) {
+                event.preventDefault();
+                event.stopPropagation();
+
+                var currentTarget = $(event.currentTarget);
+                var sessionId = currentTarget.parent().data('sessionId');
+                if ($(currentTarget).hasClass('fa-star-o')) {
+                    block_mysessions.addFavourite(currentTarget, sessionId);
+                } else {
+                    block_mysessions.removeFavourite(currentTarget, sessionId);
+                }
+            });
+
+            $('.block_mysessions .fav').keypress(function (event) {
+                if (event.which == 13) {
+                    var currentTarget = $(event.currentTarget);
+                    $(currentTarget).click();
+                }
+            });
+        },
+        /**
+         * Add images into trainings tiles
+         */
+        addImages: function () {
+            $('.block_mysessions .block-session-tile:visible').each(function () {
+                // Adding image.
+                var thumbnailDiv = $(this).find('div.session-tile-thumbnail-resize');
+                var thumbnailUrl = thumbnailDiv.attr('data-thumbnail-url');
+                thumbnailDiv.css('background-image', 'url(' + thumbnailUrl + ')');
+            });
+        },
+        /**
+         * Init session block pagination.
+         *
+         * @param {string} elementBlock
+         */
+        initPagination: function (elementBlock) {
+            pagination.initPagination(
+                $('.block_mysessions ' + elementBlock), $('#mysessions-pagination'), 12, true, this.addImages
+            );
+        },
+        /**
+         * Add session to user's favourite
+         *
+         * @param {jquery} currentTarget
+         * @param {int} sessionId
+         */
+        addFavourite: function (currentTarget, sessionId) {
+            // Call add session to user's favourite function
+            format_edadmin.ajax_call({
+                url: M.cfg.wwwroot + '/blocks/mysessions/ajax/ajax.php',
+                plugintype: 'blocks',
+                controller: 'session_favourite',
+                action: 'add_favourite',
+                format: 'json',
+                sessionid: sessionId,
+                callback: function (response) {
+
+                    response = JSON.parse(response);
+
+                    if (!response.success) {
+                        console.error(response.message);
+                        return;
+                    }
+
+                    var responseData = response.message;
+
+                    if (responseData) {
+                        // Hidden add favourite button
+                        $(currentTarget).removeClass('fa-star-o');
+                        $(currentTarget).addClass('fa-star');
+                        $(currentTarget).prop('title', M.util.get_string('removefavourite', 'block_mytrainings'));
+                    }
+                }
+            });
+        },
+        /**
+         * Add session to user's favourite
+         *
+         * @param {jquery} currentTarget
+         * @param {int} sessionId
+         */
+        removeFavourite: function (currentTarget, sessionId) {
+            // Call remove session to user's favourite function
+            format_edadmin.ajax_call({
+                url: M.cfg.wwwroot + '/blocks/mysessions/ajax/ajax.php',
+                plugintype: 'blocks',
+                controller: 'session_favourite',
+                action: 'remove_favourite',
+                format: 'json',
+                sessionid: sessionId,
+                callback: function (response) {
+
+                    response = JSON.parse(response);
+
+                    if (!response.success) {
+                        console.error(response.message);
+                        return;
+                    }
+
+                    var responseData = response.message;
+
+                    if (responseData) {
+                        // Hidden remove favourite button
+                        $(currentTarget).removeClass('fa-star');
+                        $(currentTarget).addClass('fa-star-o');
+                        $(currentTarget).prop('title', M.util.get_string('addfavourite', 'block_mytrainings'));
+                    }
+                }
+            });
+        }
+    };
+
+    //add object to window to be called outside require
+    window.block_mysessions = block_mysessions;
+    return block_mysessions;
+});
---
+++ classes/output/mysessions.php
@@ -0,0 +1,119 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Class containing data for mysessions block.
+ *
+ * @package    block_mysessions
+ * @copyright  2020 Edunao SAS (contact@edunao.com)
+ * @author     rcolet <remi.colet@edunao.com>
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+namespace block_mysessions\output;
+
+defined('MOODLE_INTERNAL') || die();
+
+use renderable;
+use renderer_base;
+use templatable;
+
+require_once($CFG->dirroot . '/local/mentor_core/lib.php');
+
+/**
+ * Class containing data for mysessions block.
+ *
+ * @copyright  2020 Edunao SAS (contact@edunao.com)
+ * @author     rcolet <remi.colet@edunao.com>
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+class mysessions implements renderable, templatable {
+
+    /**
+     * @var object An object containing the configuration information for the current instance of this block.
+     */
+    protected $config;
+
+    /**
+     * Constructor.
+     *
+     * @param object $config An object containing the configuration information for the current instance of this block.
+     */
+    public function __construct($config) {
+        $this->config = $config;
+    }
+
+    /**
+     * Export this data so it can be used as the context for a mustache template.
+     *
+     * @param \renderer_base $output
+     * @return \stdClass
+     * @throws \dml_exception
+     * @throws \moodle_exception
+     */
+    public function export_for_template(renderer_base $output) {
+        global $USER, $CFG;
+
+        require_once($CFG->dirroot . '/local/mentor_core/api/session.php');
+
+        // Get data for the block.
+        $sessionsenrol = \local_mentor_core\session_api::get_user_sessions($USER->id, false, false);
+
+        usort($sessionsenrol, "local_mentor_core_usort_favourite_session_first");
+
+        // Data for the training and session sheet.
+        $trainings = [];
+        $finalsessions = [];
+
+        // If there is at least one full session.
+        $hassessioncompleted = false;
+
+        foreach ($sessionsenrol as $sessionenrol) {
+
+            // Skip archived sessions or enrol user is enabled.
+            if (
+                $sessionenrol->status == \local_mentor_core\session::STATUS_ARCHIVED ||
+                $sessionenrol->status == \local_mentor_core\session::STATUS_CANCELLED
+            ) {
+                continue;
+            }
+
+            // Default session is not completed.
+            $sessionenrol->iscompleted = '';
+
+            // Is completed if session has completed status or user has finished his progress.
+            if (
+                (isset($sessionenrol->completed) && $sessionenrol->completed) ||
+                $sessionenrol->progress === 100
+            ) {
+                $sessionenrol->iscompleted = 'is-completed';
+                $hassessioncompleted = true;
+            }
+
+            $finalsessions[] = $sessionenrol;
+        }
+
+        // Create data for the template block.
+        $templateparams = new \stdClass();
+        $templateparams->sessions = $finalsessions;
+        $templateparams->sessionscount = count($finalsessions);
+        $templateparams->catalogurl = $CFG->wwwroot . '/local/catalog/index.php';
+        $templateparams->hassessioncompleted = $hassessioncompleted;
+
+        // Return data for the template block.
+        return $templateparams;
+    }
+}
